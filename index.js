9accdee072252382c61c9cce1ca12b1bbec03394
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-04-12 11:01:32 +0200

    Added a button to show a new quote without reloading the page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const div = document.querySelector('#simpson')
+
 function getSimpsonsQuotes(){
 
 const url = "https://simpsons-quotes-api.herokuapp.com/quotes"
@@ -11,12 +13,21 @@ axios.get(url)
 <h1>${sim[0].character}</h1>
 <img src="${sim[0].image}">
 <p>${sim[0].quote}</p>
+<button onclick="getSimpsonsQuotes()">New Quote</button>
 `
 console.log('data decoded from JSON:', sim);
 
-document.querySelector('#simpson').innerHTML = quoteHtml;
+div.innerHTML = quoteHtml;
 })
 
 }
 
 getSimpsonsQuotes()
+
+// const newQuoteButton = document.createElement("button")
+// div.appendChild(newQuoteButton)
+
+// newQuoteButton.onclick = function(){
+//     getSimpsonsQuotes()
+// }
+
